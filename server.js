76a0121470e93d3554e8a90d62f750b710ca07ec
Felix William Taylor Bjerk <felix@dontpanic.no>
2026-04-06 22:53:20 +0200

    Flytt index.html til public/ – blokker servering av server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // ── Database (Postgres) ──
 const pool = new Pool({
